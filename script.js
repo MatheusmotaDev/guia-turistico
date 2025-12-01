833ff4c13f16d1152394eb1df817ffc5c0d6a93f
MatheusMotaDev <matheusmota.webdev@gmail.com>
2025-12-01 02:39:13 -0300

    (feat) index, style and script

---
+++ script.js
@@ -0,0 +1,12 @@
+function reservarPeloZap() {
+   
+    let nome = prompt("Qual o seu nome para a reserva?");
+    
+    if (nome) {
+        let texto = "Olá! Gostaria de fazer uma reserva para o tour no Mundo Espiritual. Meu nome é " + nome;
+        let textoCodificado = encodeURIComponent(texto);
+        window.open("https://wa.me/5581988373359?text=" + textoCodificado, "_blank");
+    } else {
+        alert("Reserva cancelada.");
+    }
+}
